3e5b96ef743183e8a2c8154b8ad840736e7683e7
Dylan Dickerson <ddickerson6@nsmb208-xx.csudh.edu>
2018-03-19 16:31:49 -0700

    upload 1

---
+++ room7.c
@@ -0,0 +1,14 @@
+//Dylan Dickerson
+//CSC 251-01
+//3/19/2018
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void){
+
+puts("You find youself in a room filled with pizza and posters of Blink 182");
+
+
+
+}	
